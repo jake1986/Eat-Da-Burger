2f3631124b034c9d984b5fbab6fad3a48cd27c8c
Jake Rhoads <jake.rhoads@gmail.com>
2020-05-06 14:32:55 -0400

    initial commits

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Can't-Believe-Its-Not-Beef Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Happy Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Toilet Dash Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Belly Splitter Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", false);
